f6d9f275459d27a719f1667d45d7124bff028ff8
adixshrma1 <aditya.codez26@gmail.com>
2025-10-09 14:57:52 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { CiWarning } from 'react-icons/ci'
+
+const App = () => {
+  return (
+    <div className='text-lg bg-yellow-500'>this is a font example <CiWarning /></div>
+  )
+}
+
+export default App
